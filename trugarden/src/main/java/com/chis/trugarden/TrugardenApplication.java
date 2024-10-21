08f93317e652371b3a85973aa9c416d60a0791f7
Chris <cristru8@gmail.com>
2024-10-21 16:37:53 -0500

    first commit

---
+++ trugarden/src/main/java/com/chis/trugarden/TrugardenApplication.java
@@ -0,0 +1,13 @@
+package com.chis.trugarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrugardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrugardenApplication.class, args);
+	}
+
+}
---
+++ trugarden/src/test/java/com/chis/trugarden/TrugardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chis.trugarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrugardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
