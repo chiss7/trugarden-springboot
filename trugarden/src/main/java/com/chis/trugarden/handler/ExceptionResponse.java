f1a01d61182715e105f78034c9ef8675fb1bd713
Chris <cristru8@gmail.com>
2024-11-03 16:53:33 -0500

    add global exception handler

---
+++ trugarden/src/main/java/com/chis/trugarden/handler/ExceptionResponse.java
@@ -0,0 +1,21 @@
+package com.chis.trugarden.handler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY) // only non-empty fields will be included in the JSON output
+public class ExceptionResponse {
+    private Integer businessErrorCode;
+    private String businessErrorDescription;
+    private String error;
+    private Set<String> validationErrors;
+    private Map<String, String> errors;
+}
